810952c72032b3b1ddced0c49641d0ead3c0691d
Rosny Fletez Jr. <rfletezjr@gmail.com>
2022-10-27 15:31:21 -0700

    First Commit from STS

---
+++ BackEnd/src/main/java/com/spring/BackEnd/BackEndApplication.java
@@ -0,0 +1,15 @@
+package com.spring.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {  
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	} 
+
+}
+
+//Hello: First Commit
