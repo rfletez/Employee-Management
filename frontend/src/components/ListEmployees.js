dc304b82448b09fbaa18a4e3badd46e73257ff31
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-10-31 13:57:55 -0700

    Modified ListEmployees and App.js
    Created the empty table for employees.

--- frontend/src/components/ListEmployees.js
+++ frontend/src/components/ListEmployees.js
@@ -12,8 +12,37 @@ export default class ListEmployees extends Component {
   render() {
     return (
       <div>
-        
+        <h2 className='text-center'>Employee List</h2>
+
+        <div className='row'>
+          <table className='table table-striped table-bordered'>
+            
+            <thead>
+              <tr>
+                <th>Employee EmailID</th>
+                <th>Employee First Name</th>
+                <th>Employee Last Name</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {
+                this.state.employees.map(
+                  employee => 
+                  <tr key={employee.id}>
+                    <td> {employee.emailId} </td>
+                    <td> {employee.firstName} </td>
+                    <td> {employee.lastName} </td>
+                  </tr>
+                )
+              }
+            </tbody>
+
+          </table>
+        </div>
       </div>
     )
   }
+
 }
