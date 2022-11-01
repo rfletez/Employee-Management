e9e1ca4cef538979e84c99942aafc3bb0ce7d36c
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-11-01 08:18:05 -0700

    Updated Header and Footer
    Adding the finishing touches to both Header and Footer on App.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import ListEmployees from './components/ListEmployees';
 
 function App() {
   return (
-    <div className='container'>
-      <ListEmployees/>
+    <div>
+      <Header/>
+      <div className='container'>
+        <ListEmployees/>
+      </div>
+      <Footer/>
     </div>
   );
 }
